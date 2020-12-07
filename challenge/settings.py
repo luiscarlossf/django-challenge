f4e5912fb465aa8127880c334c2fcf364f350c0b
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-12-07 04:05:30 -0300

    Fix security issues.

--- challenge/settings.py
+++ challenge/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "djangochallenge2020@gmail.com"
-EMAIL_HOST_PASSWORD = "miio@dmin2020"
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
