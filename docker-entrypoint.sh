44334d345729ed5820031eee4432f1e11e9f38cc
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-12-07 03:52:49 -0300

    Remove spaces

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+# Apply database migrations
+echo "Apply database migrations"
+./manage.py migrate
+
+# Creating superuser
+./manage.py shell -c "from django.contrib.auth.models import User; import os; User.objects.create_superuser(os.getenv('DJANGO_USER'), os.getenv('DJANGO_EMAIL'), os.getenv('DJANGO_PASSWORD'))"
+
+# Start server
+echo "Starting server"
+./manage.py runserver 0.0.0.0:8000
