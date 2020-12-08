579d4efcb8bcd98bbe329b0fdaf12b4d4e024937
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-12-08 19:06:42 -0300

    Update env variables

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 . /root/project_env.sh
 export $(xargs < /code/.env)
 cd /code
-echo "Backuping in ${MONGODB_NAME} database..."
+echo "Backuping in ${MONGO_INITDB_DATABASE} database..."
 /usr/local/bin/python manage.py dumpdata --exclude auth > challenge.json
-mongoimport --db $MONGODB_NAME --uri mongodb://$MONGODB_USER:$MONGODB_PASSWORD@$MONGODB_HOST --jsonArray challenge.json --drop
+mongoimport --db $MONGO_INITDB_DATABASE --uri mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@mongo_db --jsonArray challenge.json --drop
 echo "Finalizing backup..."
--- challenge/settings.py
+++ challenge/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "5f@jnp1ej^!fq+5e#4-_e0re^iae%jvfby6wm*&6!#6hg@3xw="
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
