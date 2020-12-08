12b75fc3c51be9eb303ed2748acbcb087911a69e
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-12-08 01:34:56 -0300

    Redirects index path

--- challenge/urls.py
+++ challenge/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('api/', permanent=False)),
     path('', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
